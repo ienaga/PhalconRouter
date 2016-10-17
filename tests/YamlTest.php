9787f93420a3bd7831e643cffed65cd2eaaf0603
thomas <thomas@sonicmoov.com>
2016-10-18 01:47:17 +0900

    debug and develop

--- tests/YamlTest.php
+++ tests/YamlTest.php
@@ -4,33 +4,9 @@
 namespace PhalconRouter;
 
 
-require_once __DIR__ . '/../src/mvc/router/Yaml.php';
-
-
-use Exception;
-
-
 class YamlTest extends \PHPUnit_Framework_TestCase
 {
 
-
     public static function testLoad()
-    {
-
-    }
-
-    public static function testCreatePattern()
-    {
-
-    }
-
-    public static function testCreateMethod()
-    {
-
-    }
-
-    public static function testCreatePaths()
-    {
-
-    }
+    {}
 }
